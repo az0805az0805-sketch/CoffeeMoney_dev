956789862455e6e1e9bd57f04244d688ab724b94
az080 <mori@unemoloyed.com>
2026-06-03 21:38:03 +0900

    Summarya-top.htmlをcount.htmlに変更

--- src/main/java/com/example/coffeemoney/repository/CategoryRepository.java
+++ src/main/java/com/example/coffeemoney/repository/CategoryRepository.java
@@ -1,11 +1,13 @@
 package com.example.coffeemoney.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.coffeemoney.model.entity.CategoryEntity;
 
 public interface CategoryRepository extends JpaRepository<CategoryEntity, Integer> {
 
-	// カテゴリー名で検索（重複しない前提）
-	CategoryEntity findByName(String name);
+ @Query("SELECT c FROM CategoryEntity c WHERE c.name = :name")
+    CategoryEntity findByName(String name);   // カテゴリー名で検索（重複しない前提）
+    
 }
